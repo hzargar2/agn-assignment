8930dfc5449f2c5d85e8a778d3e5b99969c23908
hzargar2 <hzargar2@gmail.com>
2024-09-09 21:08:16 -0400

    improved styling of cards

--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -25,9 +25,9 @@ onBeforeMount(() => {
         <span class="flex font-medium text-center mx-auto">{{employee["Name"]}}</span>
         <span class="flex text-center mx-auto text-gray-600 {{background_text}}">{{employee["Job Title"]}}</span>
         <div class="flex flex-row flex-wrap gap-1 justify-center text-sm">
-            <span :class="`flex text-center px-1.5 rounded-2xl border-2 border-gray-600 ${background_text}`">{{employee["Department"]}}</span>
-            <span :class="`flex text-center px-1.5 rounded-2xl border-2 border-gray-600 ${background_text}`">Level {{employee["level"]}}</span>
-            <div :class="`flex flex-row px-1.5 gap-x-1 rounded-2xl border-2 border-gray-600 ${background_text}`">
+            <span :class="`flex text-center px-1.5 rounded-2xl border-2 border-gray-800 border-opacity-60 ${background_text}`">{{employee["Department"]}}</span>
+            <span :class="`flex text-center px-1.5 rounded-2xl border-2 border-gray-800 border-opacity-60 ${background_text}`">Level {{employee["level"]}}</span>
+            <div :class="`flex flex-row px-1.5 gap-x-1 rounded-2xl border-2 border-gray-800 border-opacity-60 ${background_text}`">
                 <svg class="h-3 w-3 m-auto" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M3.37892 10.2236L8 16L12.6211 10.2236C13.5137 9.10788 14 7.72154 14 6.29266V6C14 2.68629 11.3137 0 8 0C4.68629 0 2 2.68629 2 6V6.29266C2 7.72154 2.4863 9.10788 3.37892 10.2236ZM8 8C9.10457 8 10 7.10457 10 6C10 4.89543 9.10457 4 8 4C6.89543 4 6 4.89543 6 6C6 7.10457 6.89543 8 8 8Z" fill="#000000"></path> </g></svg>
                 <span class="flex text-center">{{employee["Location"]}}</span>
             </div>
