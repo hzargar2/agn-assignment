799ac8f9135bd2b9652736a00028d31e4cc4fb46
hzargar2 <hzargar2@gmail.com>
2024-09-10 01:22:30 -0400

    added functions to calculate other metrics, all metrics ca now be calculated

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,88 @@ const calculate_descendant_count = (root_employee) => {
     return count;
 }
 
+const calculate_management_cost = (root_employee) => {
+
+    let salary_sum = 0;
+
+    for (const [key, child] of Object.entries(root_employee.children)){
+        salary_sum = salary_sum + child["Salary"] + calculate_management_cost(employees_with_children[child["Employee Id"]]);
+    }
+
+    root_employee.current["management_cost"] = salary_sum;
+    return salary_sum;
+}
+
+const calculate_ic_cost = (root_employee) => {
+
+    let salary_sum = 0;
+
+    if (root_employee.children.length === 0){
+        salary_sum = root_employee.current["Salary"]
+    }
+
+    for (const [key, child] of Object.entries(root_employee.children)){
+        salary_sum = salary_sum + calculate_ic_cost(employees_with_children[child["Employee Id"]]);
+    }
+
+    root_employee.current["ic_cost"] = salary_sum;
+    return salary_sum;
+}
+
+const calculate_total_cost = (root_employee) => {
+
+    let salary_sum = root_employee.current["Salary"];
+
+    for (const [key, child] of Object.entries(root_employee.children)){
+        salary_sum = salary_sum + child["Salary"] + calculate_total_cost(employees_with_children[child["Employee Id"]]);
+    }
+
+    root_employee.current["total_cost"] = salary_sum;
+    return salary_sum;
+}
+
+// assume this is ratio of ic salaries to manager salaries and not the ration of their counts. If counts, comment this and uncomment bottom instead.
+const calculate_management_cost_ratio = (root_employee) => {
+
+    for (const [key, child] of Object.entries(root_employee.children)){
+        calculate_management_cost_ratio(employees_with_children[child["Employee Id"]]);
+    }
+
+    root_employee.current["management_cost_ratio"] = root_employee.current["ic_cost"]/root_employee.current["management_cost"];
+}
+
+// const calculate_management_cost_ratio = (root_employee) => {
+//
+//     let manager = 0;
+//
+//     if (root_employee.children.length > 0){
+//         manager++;
+//     }
+//
+//     let ic = 0;
+//
+//     if (root_employee.children.length === 0){
+//         ic++;
+//     }
+//
+//     for (const [key, child] of Object.entries(root_employee.children)){
+//         let counts = calculate_management_cost_ratio(employees_with_children[child["Employee Id"]]);
+//         manager = manager + counts[0];
+//         ic = ic + counts[1];
+//     }
+//
+//     root_employee.current["manager_count"] = manager;
+//     root_employee.current["ic_count"] = ic;
+//     root_employee.current["management_cost_ratio"] = ic/manager;
+//     return [manager, ic];
+// }
+
+
+
+
+
+
+
 onBeforeMount(async () => {
 
     // get csv data from API
@@ -52,6 +134,11 @@ onBeforeMount(async () => {
 
     // uses DFS to calculate descendant count so does it in linear time
     calculate_descendant_count(employees_with_children[0]);
+    calculate_management_cost(employees_with_children[0]);
+    calculate_ic_cost(employees_with_children[0]);
+    calculate_total_cost(employees_with_children[0]);
+    calculate_management_cost_ratio(employees_with_children[0]);
+
 
     console.log(employees_with_children);
 
