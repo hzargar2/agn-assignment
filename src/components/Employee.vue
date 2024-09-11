4ade2e9c047b61ee50ebb29b5b1a9aa0a97c8bdd
hzargar2 <hzargar2@gmail.com>
2024-09-10 22:12:56 -0400

    colored cards

--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -18,10 +18,37 @@ let background_color = ref('');
 let background_text = ref('');
 
 onBeforeMount(() => {
-    if (props.employee.current["Department"] === "Operations and Logistics"){
+    if (props.employee.current["Department"].toLowerCase().includes("operations")){
         background_color.value = "bg-[#32cd33] bg-opacity-20 hover:bg-opacity-35";
-        background_text.value = "bg-green-300";
-    }else{
+        background_text.value = "bg-[#84E184] bg-opacity-60";
+    }else if (props.employee.current["Department"].toLowerCase().includes("sales")) {
+        background_color.value = "bg-[#43A6C6] bg-opacity-20 hover:bg-opacity-35";
+        background_text.value = "bg-[#67B7D1] bg-opacity-60";
+    }else if (props.employee.current["Department"].toLowerCase().includes("data analytics")) {
+        background_color.value = "bg-[#43A6C6] bg-opacity-20 hover:bg-opacity-35";
+        background_text.value = "bg-[#67B7D1] bg-opacity-60";
+    }
+    else if (props.employee.current["Department"].toLowerCase().includes("customer support")) {
+        background_color.value = "bg-[#FFBE7C] bg-opacity-20 hover:bg-opacity-35";
+        background_text.value = "bg-[#FFA74F] bg-opacity-40";
+    }
+    else if (props.employee.current["Department"].toLowerCase().includes("system")) {
+        background_color.value = "bg-[#FF8A8A] bg-opacity-20 hover:bg-opacity-35";
+        background_text.value = "bg-[#FF5C5C] bg-opacity-40";
+    }
+    else if (props.employee.current["Department"].toLowerCase().includes("project")) {
+        background_color.value = "bg-[#D396FF] bg-opacity-20 hover:bg-opacity-35";
+        background_text.value = "bg-[#BF68FF] bg-opacity-40";
+    }
+    else if (props.employee.current["Department"].toLowerCase().includes("human resources")) {
+        background_color.value = "bg-[#F5F5DC] bg-opacity-20 hover:bg-opacity-35";
+        background_text.value = "bg-[#EBEBB8] bg-opacity-40";
+    }
+    else if (props.employee.current["Department"].toLowerCase().includes("software")) {
+        background_color.value = "bg-[#F5F5DC] bg-opacity-20 hover:bg-opacity-35";
+        background_text.value = "bg-[#EBEBB8] bg-opacity-40";
+    }
+    else{
         background_color.value = "bg-white hover:bg-gray-100";
         background_text.value = "bg-gray-200";
     }
@@ -32,17 +59,22 @@ onBeforeMount(() => {
 <template>
     <div class="flex flex-col gap-y-16">
 
-        <div :class="`justify-center m-auto flex flex-col w-48 h-56 p-4 gap-y-2 bg-white border border-gray-200 rounded-lg shadow hover:cursor-pointer ${background_color}`" @click="expanded = !expanded">
+        <div :class="`justify-center m-auto flex flex-col w-60 h-80 p-4 gap-y-2 bg-white border border-gray-200 rounded-lg shadow hover:cursor-pointer ${background_color}`" @click="expanded = !expanded">
 
             <div class="flex flex-col gap-y-1 mt-0 m-auto">
                 <span class="flex font-medium mx-auto">{{props.employee.current["Name"]}}</span>
                 <span class="flex text-center mx-auto text-gray-600 {{background_text}}">{{props.employee.current["Job Title"]}}</span>
             </div>
 
-            <div class="flex flex-row flex-wrap gap-1 mb-0 m-auto text-sm">
-                <span :class="`flex text-center px-1.5 rounded-2xl border-2 border-gray-800 border-opacity-60 ${background_text}`">{{props.employee.current["Department"]}}</span>
-                <span :class="`flex text-center px-1.5 rounded-2xl ${background_text}`">Level {{props.employee.current["level"]}}</span>
-                <div :class="`flex flex-row px-1.5 gap-x-1 rounded-2xl ${background_text}`">
+            <div class="flex flex-col gap-1 mb-0 m-auto text-sm">
+<!--                Calculate numeric values for styling inline-->
+                <span :class="`flex text-center px-2 py-0.5 rounded-2xl m-auto border-2 border-gray-800 border-opacity-60 ${background_text}`">{{props.employee.current["Department"]}}</span>
+                <span :class="`flex text-center px-2 py-0.5 rounded-2xl m-auto ${background_text}`">Level {{props.employee.current["level"]}}</span>
+                <span :class="`flex text-center px-2 py-0.5 rounded-2xl m-auto ${background_text}`">Management cost: ${{ props.employee.current["management_cost"] > 1000000 ? (Math.round((props.employee.current["management_cost"] + Number.EPSILON)/1000000 * 100) / 100) + "M" : (Math.round((props.employee.current["management_cost"]/1000 + Number.EPSILON) * 100) / 100) + "K" }}</span>
+                <span :class="`flex text-center px-2 py-0.5 rounded-2xl m-auto ${background_text}`">IC cost: ${{ props.employee.current["ic_cost"] > 1000000 ? (Math.round((props.employee.current["ic_cost"] + Number.EPSILON)/1000000 * 100) / 100) + "M" : (Math.round((props.employee.current["ic_cost"]/1000 + Number.EPSILON) * 100) / 100) + "K" }}</span>
+                <span :class="`flex text-center px-2 py-0.5 rounded-2xl m-auto ${background_text}`">Total cost: ${{ props.employee.current["total_cost"] > 1000000 ? (Math.round((props.employee.current["total_cost"] + Number.EPSILON)/1000000 * 100) / 100) + "M" : (Math.round((props.employee.current["total_cost"]/1000 + Number.EPSILON) * 100) / 100) + "K" }}</span>
+                <span :class="`flex text-center px-2 py-0.5 rounded-2xl m-auto ${background_text}`">Management cost ratio: {{Math.round((props.employee.current["management_cost_ratio"] + Number.EPSILON) * 100) / 100}}</span>
+                <div :class="`flex flex-row px-2 py-0.5 gap-x-1 rounded-2xl m-auto ${background_text}`">
                     <svg class="h-3 w-3 m-auto" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M3.37892 10.2236L8 16L12.6211 10.2236C13.5137 9.10788 14 7.72154 14 6.29266V6C14 2.68629 11.3137 0 8 0C4.68629 0 2 2.68629 2 6V6.29266C2 7.72154 2.4863 9.10788 3.37892 10.2236ZM8 8C9.10457 8 10 7.10457 10 6C10 4.89543 9.10457 4 8 4C6.89543 4 6 4.89543 6 6C6 7.10457 6.89543 8 8 8Z" fill="#000000"></path> </g></svg>
                     <span class="flex text-center">{{props.employee.current["Location"]}}</span>
                 </div>
