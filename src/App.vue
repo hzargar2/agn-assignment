db6b09ba0893970cc94abfe56217cca367894e34
hzargar2 <hzargar2@gmail.com>
2024-09-11 23:49:35 -0400

    improved loading screen

--- src/App.vue
+++ src/App.vue
@@ -60,15 +60,19 @@ onMounted(async () => {
         </div>
 
         <div class="flex w-screen h-screen" v-if="failed_to_fetch_data && data_ready">
-            <span class="flex gap-y-4 flex-col w-fit h-fit m-auto text-center p-4 bg-red-500 bg-opacity-90 text-slate-100 rounded-xl">
+            <span class="flex gap-y-4 flex-col w-fit h-fit m-auto text-center p-4 bg-rose-500 text-slate-100 rounded-xl">
                 <span class="text-4xl">Failed to load CSV file</span>
                 <span class="text-2xl">Go to http://localhost:3000/employees to see if there is a response.</span>
             </span>
         </div>
 
         <div class="flex w-screen h-screen" v-else-if="!failed_to_fetch_data && !data_ready">
-            <span class="flex gap-y-4 flex-col w-fit h-fit m-auto text-center p-4 bg-red-500 bg-opacity-90 text-slate-100 rounded-xl">
-                <span class="text-4xl">Loading...</span>
+            <span class="flex flex-row gap-y-4 w-fit h-fit m-auto text-center p-4 bg-emerald-500 text-slate-100 rounded-xl">
+                <svg class="m-auto animate-spin -ml-1 mr-3 h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                  <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                <span class="m-auto text-2xl">Loading...</span>
             </span>
         </div>
 
