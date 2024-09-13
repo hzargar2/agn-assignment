24c2a455b475b3a1ca91884245d70a428b3b1494
hzargar2 <hzargar2@gmail.com>
2024-09-13 19:12:46 -0400

    added location pin, now works with d3 js

--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {onBeforeMount, onMounted, ref} from "vue";
+import * as d3 from "d3";
 
 let props = defineProps({
     employee: {
@@ -67,10 +68,11 @@ onBeforeMount(() => {
             <span :class="`flex text-center px-2 py-0.5 rounded-2xl m-auto ${background_text}`">IC cost: ${{props.employee.data["ic_cost"] > 1000000000 ? (Math.round((props.employee.data["ic_cost"] + Number.EPSILON) / 1000000000 * 100) / 100) + "B" : props.employee.data["ic_cost"] > 1000000 ? (Math.round((props.employee.data["ic_cost"] + Number.EPSILON) / 1000000 * 100) / 100) + "M" : (Math.round((props.employee.data["ic_cost"] / 1000 + Number.EPSILON) * 100) / 100) + "K"}}</span>
             <span v-if="props.employee.data['descendant_count'] > 0" :class="`flex text-center px-2 py-0.5 rounded-2xl m-auto ${background_text}`">Total cost: ${{props.employee.data["total_cost"] > 1000000000 ? (Math.round((props.employee.data["total_cost"] + Number.EPSILON) / 1000000000 * 100) / 100) + "B" :props.employee.data["total_cost"] > 1000000 ? (Math.round((props.employee.data["total_cost"] + Number.EPSILON) / 1000000 * 100) / 100) + "M" : (Math.round((props.employee.data["total_cost"] / 1000 + Number.EPSILON) * 100) / 100) + "K"}}</span>
             <span v-if="props.employee.data['descendant_count'] > 0" :class="`flex text-center px-2 py-0.5 rounded-2xl m-auto ${background_text}`">Management cost ratio: {{Math.round((props.employee.data["management_cost_ratio"] + Number.EPSILON) * 100) / 100}}</span>
-            <div :class="`flex flex-row px-2 py-0.5 gap-x-1 rounded-2xl m-auto ${background_text}`">
-<!--                <svg class="h-3 w-3 m-auto" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M3.37892 10.2236L8 16L12.6211 10.2236C13.5137 9.10788 14 7.72154 14 6.29266V6C14 2.68629 11.3137 0 8 0C4.68629 0 2 2.68629 2 6V6.29266C2 7.72154 2.4863 9.10788 3.37892 10.2236ZM8 8C9.10457 8 10 7.10457 10 6C10 4.89543 9.10457 4 8 4C6.89543 4 6 4.89543 6 6C6 7.10457 6.89543 8 8 8Z" fill="#000000"></path> </g></svg>-->
+            <span :class="`flex flex-row px-2 py-0.5 gap-x-1 rounded-2xl m-auto ${background_text}`">
+<!--                Need to store svg as file and import using html img attribute, rending an svg inside an svg in the d3 chart doesn't work since it is xml, d3.xml() might also work, use this method instead-->
+                <img class="h-3 w-3 m-auto" src="@/assets/location_pin.svg" alt="location pin"/>
                 <span class="flex text-center">{{props.employee.data["Location"]}}</span>
-            </div>
+            </span>
         </div>
 <!--        <div v-if="children.length > 0" class="bg-slate-700 rounded-3xl text-white w-fit px-2 py-0.5 m-auto">-->
 <!--            <span class="text-center">{{children.length}}/{{props.employee.data["descendant_count"]}}</span>-->
